20f605435d893330f8d218923c8dbe868644acaf
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-27 22:09:45 -0400

    Feat: Create views structure
    Created views required for polling app. Also, added the urls to access
    those views.

--- polls/views.py
+++ polls/views.py
@@ -7,3 +7,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello, World. You are at the polls index!')
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s.' %question_id)
+
+def results(request, question_id):
+    result = "You're looking at the results of question %s."
+    return HttpResponse(result %question_id)
+
+def vote(request, question_id):
+    return HttpResponse('You are voting for question %s.' %question_id)
