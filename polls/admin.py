27133a52d485736534d03217bb2d3e2bd6d64b37
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-04-03 22:45:45 -0400

    Feat: Add published status for questions

--- polls/admin.py
+++ polls/admin.py
@@ -15,6 +15,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Information', {'fields':['pub_date']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
